5f0a9fda636dc99ea3a55934c980fed250be8e96
Janet <jbembe12@yahoo.com>
2020-08-07 16:37:20 +0200

    entity meal created

---
+++ app/src/main/java/com/example/newborn/bo/Meal.java
@@ -0,0 +1,138 @@
+package com.example.newborn.bo;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDateTime;
+
+/**
+ * Class representing a meal
+ */
+@Entity
+public class Meal implements Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int baby;
+    private LocalDateTime time;
+    private int quantity;
+    // right = true, left = false
+    private boolean rightBreast;
+
+    //constructors
+    public Meal(int id, int baby, LocalDateTime time, int quantity, boolean rightBreast) {
+        this.id = id;
+        this.baby = baby;
+        this.time = time;
+        this.quantity = quantity;
+        this.rightBreast = rightBreast;
+    }
+
+    public Meal(int id, int baby, LocalDateTime time) {
+        this.id = id;
+        this.baby = baby;
+        this.time = time;
+    }
+
+    public Meal(int id, int baby, LocalDateTime time, int quantity) {
+        this.id = id;
+        this.baby = baby;
+        this.time = time;
+        this.quantity = quantity;
+    }
+
+    public Meal(int id, int baby, LocalDateTime time, boolean rightBreast) {
+        this.id = id;
+        this.baby = baby;
+        this.time = time;
+        this.rightBreast = rightBreast;
+    }
+
+    protected Meal(Parcel in) {
+        id = in.readInt();
+        baby = in.readInt();
+        quantity = in.readInt();
+        rightBreast = in.readByte() != 0;
+    }
+
+    public static final Creator<Meal> CREATOR = new Creator<Meal>() {
+        @Override
+        public Meal createFromParcel(Parcel in) {
+            return new Meal(in);
+        }
+
+        @Override
+        public Meal[] newArray(int size) {
+            return new Meal[size];
+        }
+    };
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBaby() {
+        return baby;
+    }
+
+    public void setBaby(int baby) {
+        this.baby = baby;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isRightBreast() {
+        return rightBreast;
+    }
+
+    public void setRightBreast(boolean rightBreast) {
+        this.rightBreast = rightBreast;
+    }
+    //to String
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", baby=" + baby +
+                ", time=" + time +
+                ", quantity=" + quantity +
+                ", rightBreast=" + rightBreast +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeInt(baby);
+        parcel.writeInt(quantity);
+        parcel.writeByte((byte) (rightBreast ? 1 : 0));
+    }
+}
