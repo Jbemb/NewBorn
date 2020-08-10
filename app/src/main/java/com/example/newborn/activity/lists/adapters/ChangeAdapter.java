0307662ce383efaba57620266052043c2a8f54b4
Janet <jbembe12@yahoo.com>
2020-08-10 17:37:44 +0200

    unfinished change adapter

--- app/src/main/java/com/example/newborn/activity/lists/adapters/ChangeAdapter.java
+++ app/src/main/java/com/example/newborn/activity/lists/adapters/ChangeAdapter.java
@@ -5,15 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.newborn.R;
+import com.example.newborn.change.bo.Change;
 
 import java.util.List;
 
-public class ChangeAdapter extends ArrayAdapter {
+public class ChangeAdapter extends ArrayAdapter<Change> {
     public ChangeAdapter(@NonNull Context context, int resource, @NonNull List objects) {
         super(context, resource, objects);
     }
@@ -26,6 +29,16 @@ public class ChangeAdapter extends ArrayAdapter {
             convertView = li.inflate(R.layout.style_list_change, parent, false);
         }
         //TODO add info by line
+        TextView tvDate = convertView.findViewById(R.id.tv_date);
+        TextView tvTime = convertView.findViewById(R.id.tv_time);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_selle);
+
+        Change change = getItem(position);
+
+        
+        if (change.getPoop()){
+            imageView.setVisibility(View.VISIBLE);
+        }
         return convertView;
     }
 }
