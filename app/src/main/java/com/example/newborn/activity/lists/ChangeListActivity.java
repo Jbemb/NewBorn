f402904ee4dcae1c6779dadacb3e65b5680c25e7
Janet <jbembe12@yahoo.com>
2020-08-10 18:58:18 +0200

    change list activity
    save before run

--- app/src/main/java/com/example/newborn/activity/lists/ChangeListActivity.java
+++ app/src/main/java/com/example/newborn/activity/lists/ChangeListActivity.java
@@ -1,20 +1,41 @@
 package com.example.newborn.activity.lists;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import com.example.newborn.R;
+import com.example.newborn.activity.lists.adapters.ChangeAdapter;
+import com.example.newborn.change.bo.Change;
+import com.example.newborn.change.view_model.ChangeViewModel;
+
+import java.util.List;
 
 public class ChangeListActivity extends AppCompatActivity {
 
+    private ChangeViewModel changeVM = null;
+    ListView changeList =null;
+    List<Change> changes = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_list);
+
+        changeList = findViewById(R.id.lv_changeList);
+        changeVM = ViewModelProviders.of(this).get(ChangeViewModel.class);
+        changeVM.getObserverChangeByBaby().observe(this, new Observer<List<Change>>() {
+            @Override
+            public void onChanged(List<Change> changes) {
+            ChangeListActivity.this.changes = changes;
+            changeList.setAdapter(new ChangeAdapter(ChangeListActivity.this,R.layout.style_list_change,changes));
+            }
+        });
     }
 
     public void OnClickAddChange(View view) {
--- app/src/main/java/com/example/newborn/change/view_model/ChangeViewModel.java
+++ app/src/main/java/com/example/newborn/change/view_model/ChangeViewModel.java
@@ -21,35 +21,35 @@ public class ChangeViewModel extends AndroidViewModel {
         changeRepo = new ChangeDbRepository(application);
     }
 
-    LiveData<List<Change>> getObserverChangeByBaby() {
+    public LiveData<List<Change>> getObserverChangeByBaby() {
         return changeRepo.getObserverChangeByBaby();
     }
 
-    LiveData<List<Change>> getObserverChangeByBabyByDate() {
+    public LiveData<List<Change>> getObserverChangeByBabyByDate() {
         return changeRepo.getObserverChangeByBabyByDate();
     }
 
-    LiveData<Change> getObserverLastChangeByBaby() {
+    public LiveData<Change> getObserverLastChangeByBaby() {
         return changeRepo.getObserverLastChangeByBaby();
     }
 
-    void getChangeByBabyByDate(String baby, LocalDateTime date) {
+    public void getChangeByBabyByDate(String baby, LocalDateTime date) {
         changeRepo.getChangeByBabyByDate(baby, date);
     }
 
-    void getLastChangeByBaby(String baby) {
+    public void getLastChangeByBaby(String baby) {
         changeRepo.getLastChangeByBaby(baby);
     }
 
-    void insertChange(Change change) {
+    public void insertChange(Change change) {
         changeRepo.insertChange(change);
     }
 
-    void updateChange(Change change) {
+    public void updateChange(Change change) {
         changeRepo.updateChange(change);
     }
 
-    void deleteChange(Change change) {
+    public void deleteChange(Change change) {
         changeRepo.deleteChange(change);
     }
 }
