f94592bb16a61a2ca5f9a0308614a97b56a9f06b
Amandine Foulet <amandine.foulet@gmail.com>
2020-08-10 11:45:51 +0200

    temp commit

---
+++ app/src/main/java/com/example/newborn/appRepository/DoubleParamAsync.java
@@ -0,0 +1,16 @@
+package com.example.newborn.appRepository;
+
+import java.time.LocalDateTime;
+
+/**
+ * This class is used to pass two parameters to AsyncTask
+ */
+public class DoubleParamAsync {
+    String baby;
+    LocalDateTime date;
+
+    public DoubleParamAsync(String baby, LocalDateTime date) {
+        this.baby = baby;
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/com/example/newborn/change/repository/ChangeDbRepository.java
@@ -0,0 +1,107 @@
+package com.example.newborn.change.repository;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.newborn.appRepository.DoubleParamAsync;
+import com.example.newborn.change.bo.Change;
+import com.example.newborn.change.dal.ChangeDao;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ChangeDbRepository implements IChangeRepository{
+
+    LiveData<List<Change>> observerChangeByBaby = null;
+    MutableLiveData<List<Change>> observerChangeByBabyByDate = null;
+    MutableLiveData<Change> observerLastChangeByBaby = null;
+
+    ChangeDao dao = null;
+    //TODO: change to real names in the baby file
+    String baby = "Zachary";
+
+    public ChangeDbRepository(Context context) {
+        AppDatabase database = AppDatabase.getInstance(context);
+        dao = database.getChangeDao();
+
+        //TODO: change to real names in the baby file
+        observerChangeByBaby = dao.getChangeByBaby(baby);
+
+        observerChangeByBabyByDate = new MutableLiveData<>();
+        observerLastChangeByBaby = new MutableLiveData<>();
+    }
+    @Override
+    public LiveData<List<Change>> getObserverChangeByBaby() {
+        return observerChangeByBaby;
+    }
+
+    @Override
+    public LiveData<List<Change>> getObserverChangeByBabyByDate() {
+        return observerChangeByBabyByDate;
+    }
+
+    @Override
+    public LiveData<Change> getObserverLastChangeByBaby() {
+        return observerLastChangeByBaby;
+    }
+
+    @Override
+    public void getChangeByBabyByDate(String baby, LocalDateTime date) {
+        DoubleParamAsync babyDateParams = new DoubleParamAsync(baby, date);
+        new AsyncTask<DoubleParamAsync,Void,List<Change>>() {
+
+            @Override
+            protected List<Change> doInBackground(DoubleParamAsync... babyDateParams) {
+                String baby = babyDateParams[0];
+                LocalDateTime date = babyDateParams[0];
+
+                return dao.getChangeByBabyByDate(baby, date);
+            }
+
+            @Override
+            protected void onPostExecute(List<Change> change) {
+                super.onPostExecute(change);
+                observerChangeByBabyByDate.setValue(change);
+            }
+        }.execute(baby, date);
+    }
+
+    @Override
+    public void getLastChangeByBaby(String baby) {
+
+    }
+
+
+    @Override
+    public void insertChange(final Change change) {
+        AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.insertChange(change);
+            }
+        });
+    }
+
+    @Override
+    public void updateChange(final Change change) {
+        AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.updateChange(change);
+            }
+        });
+    }
+
+    @Override
+    public void deleteChange(final Change change) {
+        AppDatabase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.deleteChange(change);
+            }
+        });
+    }
+}
