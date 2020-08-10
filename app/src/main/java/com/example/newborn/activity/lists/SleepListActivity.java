65e42c62425b3f622c259397411a272afa7e0ba1
Amandine Foulet <amandine.foulet@gmail.com>
2020-08-10 17:50:45 +0200

    add sleep activity and adapter

--- app/src/main/java/com/example/newborn/activity/lists/SleepListActivity.java
+++ app/src/main/java/com/example/newborn/activity/lists/SleepListActivity.java
@@ -1,16 +1,43 @@
 package com.example.newborn.activity.lists;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.newborn.R;
+import com.example.newborn.activity.lists.adapters.SleepAdapter;
+import com.example.newborn.sleep.bo.Sleep;
+import com.example.newborn.sleep.view_model.SleepViewModel;
+
+import java.util.List;
 
 public class SleepListActivity extends AppCompatActivity {
 
+    private SleepViewModel sleepVM = null;
+    ListView sleepList =null;
+    List<Sleep> sleeps = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_list);
+
+        sleepList = findViewById(R.id.lv_sleeplist);
+
+        sleepVM = ViewModelProviders.of(this).get(SleepViewModel.class);
+
+        sleepVM.getObserverSleepByBaby().observe(this, new Observer<List<Sleep>>() {
+            @Override
+            public void onChanged(List<Sleep> sleeps) {
+                SleepListActivity.this.sleeps = sleeps;
+                sleepList.setAdapter(new SleepAdapter(SleepListActivity.this,R.layout.style_list_sleep,sleeps));
+            }
+        });
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/newborn/activity/lists/adapters/SleepAdapter.java
@@ -0,0 +1,69 @@
+package com.example.newborn.activity.lists.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.newborn.R;
+import com.example.newborn.sleep.bo.Sleep;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
+public class SleepAdapter extends ArrayAdapter<Sleep> {
+    public SleepAdapter(@NonNull Context context, int resource, @NonNull List<Sleep> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if(convertView == null) {
+            LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = li.inflate(R.layout.style_list_sleep, parent,false);
+        }
+        //Get the sleep item at the line position
+        Sleep sleep = getItem(position);
+
+        LocalDateTime dateTimeSleepStart = sleep.getStartTime();
+
+        //Set the date to be showed
+        TextView tvDate = convertView.findViewById(R.id.tv_dateItem);
+        String dateString = new SimpleDateFormat("dd/MM/yyyy").format(dateTimeSleepStart);
+        tvDate.setText(dateString);
+
+        //Set the time to be showed
+        TextView tvTime = convertView.findViewById(R.id.tv_timeItem);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String timeString = dateTimeSleepStart.format(formatter);
+        tvTime.setText(timeString);
+
+        //Calculation of the duration of sleep
+        LocalDateTime dateTimeSleepEnd = sleep.getEndTime();
+        Duration durationMilliseconds = Duration.between(dateTimeSleepEnd, dateTimeSleepStart);
+        long duration = Math.abs(durationMilliseconds.toMinutes());
+
+        TextView tvDuration = convertView.findViewById(R.id.tv_durationItem);
+
+        if (duration < 60) {
+            tvDuration.setText(duration + " min");
+        } else {
+            long hours = durationMilliseconds.toHours();
+            long minutes = durationMilliseconds.minusHours(hours).toMinutes();
+            tvDuration.setText(hours + " h " + minutes);
+        }
+        
+        return convertView;
+    }
+}
