8049e3e6982e079e63f56daab803dd246773b429
Janet <jbembe12@yahoo.com>
2020-08-10 15:49:09 +0200

    adapter without line information

--- app/src/main/java/com/example/newborn/activity/lists/adapters/ChangeAdapter.java
+++ app/src/main/java/com/example/newborn/activity/lists/adapters/ChangeAdapter.java
@@ -1,4 +1,31 @@
 package com.example.newborn.activity.lists.adapters;
 
-public class ChangeAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.newborn.R;
+
+import java.util.List;
+
+public class ChangeAdapter extends ArrayAdapter {
+    public ChangeAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = li.inflate(R.layout.line_style_change, parent, false);
+        }
+        //TODO add info by line
+        return convertView;
+    }
 }
