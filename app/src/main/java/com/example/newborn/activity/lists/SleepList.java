5873601eb90c3f76788cad692643d6b0d356f423
Amandine Foulet <amandine.foulet@gmail.com>
2020-08-10 15:06:44 +0200

    create package lists in activity + acitivty sleep list

---
+++ app/src/main/java/com/example/newborn/activity/lists/SleepList.java
@@ -0,0 +1,16 @@
+package com.example.newborn.activity.lists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newborn.R;
+
+public class SleepList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_list);
+    }
+}
