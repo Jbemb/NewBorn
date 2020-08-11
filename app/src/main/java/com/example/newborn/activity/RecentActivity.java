8f1b2707a0f5fff7d82707e904b4eace6f74d1b1
Janet <jbembe12@yahoo.com>
2020-08-11 16:33:35 +0200

    recent activity

--- app/src/main/java/com/example/newborn/activity/RecentActivity.java
+++ app/src/main/java/com/example/newborn/activity/RecentActivity.java
@@ -3,6 +3,10 @@ package com.example.newborn.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.newborn.R;
+import com.facebook.stetho.Stetho;
 
 public class RecentActivity extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class RecentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recent);
+        Stetho.initializeWithDefaults(this);
+    }
+
+    public void onClickAddMeal(View view) {
     }
 }
