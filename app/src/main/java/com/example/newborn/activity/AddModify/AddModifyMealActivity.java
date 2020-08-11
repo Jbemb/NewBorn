ca12747da67ac7e44e0e85e45173bfc551ae5795
Janet <jbembe12@yahoo.com>
2020-08-11 10:30:08 +0200

    addmodify meal activity

---
+++ app/src/main/java/com/example/newborn/activity/AddModify/AddModifyMealActivity.java
@@ -0,0 +1,14 @@
+package com.example.newborn.activity.AddModify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddModifyMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_modify_meal);
+    }
+}
