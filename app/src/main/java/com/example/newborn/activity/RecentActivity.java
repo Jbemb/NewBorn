356dc7f55ce6f58bae8f574858a070871948c5cd
Janet <jbembe12@yahoo.com>
2020-08-12 12:38:29 +0200

    functioning chronometer

--- app/src/main/java/com/example/newborn/activity/RecentActivity.java
+++ app/src/main/java/com/example/newborn/activity/RecentActivity.java
@@ -5,12 +5,16 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.newborn.R;
 import com.example.newborn.change.bo.Change;
@@ -26,22 +30,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+
 public class RecentActivity extends AppCompatActivity {
     //baby name to change later and other "parameters"
     private String baby = "Zachary";
     private boolean isBreastFeeding = true;
     private boolean isBottle = true;
-    //timers
-
+    //timer sleep
+    private Chronometer sleepChrono;
+    private boolean isSleeping;
+    private long newSleepDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recent);
         Stetho.initializeWithDefaults(this);
-        final TextView tvMealTime = findViewById(R.id.tv_meal_time);
-        final TextView tvChangeTime = findViewById(R.id.tv_change_time);
-        final TextView tvSleepCount = findViewById(R.id.tv_sleep_count);
+
         //set day
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date date = new Date();
@@ -49,30 +54,32 @@ public class RecentActivity extends AppCompatActivity {
         tvToday.setText(dateFormat.format(date));
 
         //Meal
+        final TextView tvMealTime = findViewById(R.id.tv_meal_time);
         final TextView tvMealQuantity = findViewById(R.id.tv_recent_meal_quantity);
         final TextView tvMealBoob = findViewById(R.id.tv_recent_boob);
         final EditText etQuantity = findViewById(R.id.et_meal_quantity);
         final RadioGroup rgBreast = findViewById(R.id.rg_breast);
         final RadioButton rbLeft = findViewById(R.id.rb_left);
         final RadioButton rbright = findViewById(R.id.rb_right);
-        final ImageButton btnStartMeal = findViewById(R.id.btn_new_change);
-
-        //countdown timer meal
-       /*
-        btnStartMeal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startTimer();
-            }
-        });
-
-        */
+        //final ImageButton btnStartMeal = findViewById(R.id.btn_new_change);
 
         MealViewModel mvm = new ViewModelProvider(this).get(MealViewModel.class);
         mvm.getLastMealByBaby(baby);
         mvm.getObserverLastMealByBaby().observe(this, new Observer<Meal>() {
             @Override
             public void onChanged(Meal meal) {
+                //meal timer
+                Date dateTimeMeal = meal.getTime();
+                Date now = new Date();
+                long timeSinceMilliseconds =  now.getTime() - dateTimeMeal.getTime();
+                long timeSince = timeSinceMilliseconds/1000/60;
+                if (timeSince < 60) {
+                    tvMealTime.setText("00 : " + timeSince);
+                } else {
+                    long hours = timeSince/60;
+                    long minutes = timeSince%60;
+                    tvMealTime.setText(hours + " : " + minutes);
+                }
 
                 if(meal.getQuantity()!=0){
                     tvMealQuantity.setVisibility(View.VISIBLE);
@@ -108,6 +115,7 @@ public class RecentActivity extends AppCompatActivity {
         });
 
         //set couche
+        final TextView tvChangeTime = findViewById(R.id.tv_change_time);
         final TextView tvselle = findViewById(R.id.tv_selle);
 
         ChangeViewModel cvm = new ViewModelProvider(this).get(ChangeViewModel.class);
@@ -115,6 +123,19 @@ public class RecentActivity extends AppCompatActivity {
         cvm.getObserverLastChangeByBaby().observe(this, new Observer<Change>() {
             @Override
             public void onChanged(Change change) {
+                //set time
+                Date dateTimeChange = change.getChangeTime();
+                Date now = new Date();
+                long timeSinceMilliseconds =  now.getTime() - dateTimeChange.getTime();
+                long timeSince = timeSinceMilliseconds/1000/60;
+                if (timeSince < 60) {
+                    tvChangeTime.setText("Dernier change: 00 : " + timeSince);
+                } else {
+                    long hours = timeSince/60;
+                    long minutes = timeSince%60;
+                    tvChangeTime.setText("Dernier change: "+ hours + " : " + minutes);
+                }
+                //set selle
                 String selle = "non";
                 if(change.getPoop()){
                     selle= "oui";
@@ -122,10 +143,12 @@ public class RecentActivity extends AppCompatActivity {
                 tvselle.setText("selle: " + selle);
             }
         });
-
         //set dodo
+        sleepChrono = findViewById(R.id.chrono_sleep_count);
         final TextView tvSleepTime = findViewById(R.id.tv_sleep_time);
         final TextView tvSleepDuration = findViewById(R.id.tv_sleep_duration);
+        final ImageButton btnStartSleep = findViewById(R.id.btn_start_sleep);
+        final ImageButton btnStopSleep = findViewById(R.id.btn_stop_sleep);
 
         SleepViewModel svm = new ViewModelProvider(this).get(SleepViewModel.class);
         svm.getLastSleepByBaby(baby);
@@ -138,11 +161,11 @@ public class RecentActivity extends AppCompatActivity {
                 long timeSinceMilliseconds =  now.getTime() - dateTimeSleepEnd.getTime();
                 long timeSince = timeSinceMilliseconds/1000/60;
                 if (timeSince < 60) {
-                    tvSleepTime.setText("Dernier dodo: " + timeSince + " min");
+                    tvSleepTime.setText("Dernier dodo: 00 : " + timeSince);
                 } else {
                     long hours = timeSince/60;
                     long minutes = timeSince%60;
-                    tvSleepTime.setText("Dernier dodo: "+ hours + " h " + minutes);
+                    tvSleepTime.setText("Dernier dodo: "+ hours + " : " + minutes);
                 }
                 //Calculation of the duration of sleep
                 Date dateTimeSleepStart = sleep.getStartTime();
@@ -155,10 +178,9 @@ public class RecentActivity extends AppCompatActivity {
                     long minutes = duration%60;
                     tvSleepDuration.setText("durée: " + hours + " h " + minutes);
                 }
-                //sleep count
-
             }
         });
+
     }
 
     public void onClickAddMeal(View view) {
@@ -170,6 +192,23 @@ public class RecentActivity extends AppCompatActivity {
     public void checkButton(View view) {
     }
 
-    public void onClickNewSleep(View view) {
+    public void startSleepTimer(View view) {
+        if(!isSleeping){
+            sleepChrono.setBase(SystemClock.elapsedRealtime());
+            sleepChrono.start();
+            isSleeping = true;
+        }
     }
+
+    public void stopSleepTimer(View view) {
+        if(isSleeping){
+            sleepChrono.stop();
+            newSleepDuration = SystemClock.elapsedRealtime() - sleepChrono.getBase();
+            isSleeping = false;
+            //reset chrono to 0
+            sleepChrono.setBase(SystemClock.elapsedRealtime());
+            Toast.makeText(this, "time" + newSleepDuration, Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
