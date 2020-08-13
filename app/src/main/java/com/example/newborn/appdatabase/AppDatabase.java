fe87b0198286eae008075c8c3d2f0fa8ea5b4d20
Amandine Foulet <amandine.foulet@gmail.com>
2020-08-13 18:07:45 +0200

    a few improvements on the UI

--- app/src/main/java/com/example/newborn/appdatabase/AppDatabase.java
+++ app/src/main/java/com/example/newborn/appdatabase/AppDatabase.java
@@ -51,7 +51,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getInstance(Context context){
         if(INSTANCE == null){
             //creation - context (who access it) - this class info - database name
-            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, "izzy_baby10.db")
+            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, "izzy_baby.db")
                     .addCallback(roomFixture)
                     .fallbackToDestructiveMigration()
                     .build();
